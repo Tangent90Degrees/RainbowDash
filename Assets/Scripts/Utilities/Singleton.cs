91ebc59c9d8add4414e32c1c7173cb16f5a3c7b6
Tangent90Degrees <victorliu0530us@icloud.com>
2023-06-13 12:05:01 +0800

    Singleton.

---
+++ Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Base class of all singletons.
+/// </summary>
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+
+    /// <summary>
+    /// The only instance of this Singleton.
+    /// </summary>
+    public static T Instance => _instance;
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            _instance = this as T;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            _instance = null;
+        }
+    }
+
+    private static T _instance;
+
+}
